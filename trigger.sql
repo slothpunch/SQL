abd4f1a1d92c19e12d254e84dce817f3f259a6e8
slothpunch <42757351+slothpunch@users.noreply.github.com>
2021-09-01 20:25:27 +1000

    Add files via upload

--- trigger.sql
+++ trigger.sql
@@ -59,3 +59,64 @@ WHERE
 	emp_no = '10001'
 		AND from_date = '2010-06-22';
 
+SELECT SYSDATE(); -- 2021-09-01 14:52:02
+
+SELECT DATE_FORMAT(SYSDATE(), '%Y-%M-%D') AS today; -- 2021-September-1st
+SELECT DATE_FORMAT(SYSDATE(), '%y-%m-%d') AS today; -- 21-09-01
+
+DELIMITER $$
+
+CREATE TRIGGER trig_ins_dep_mng -- trigger for inserting department managers
+AFTER INSERT ON employees.dept_manager -- activated whenever a new data is inserted into dept_manager
+FOR EACH ROW -- check the status of data on all rows
+BEGIN
+	DECLARE v_curr_salary INT;
+
+SELECT 
+    MAX(salary)
+INTO v_curr_salary FROM
+    salaries
+WHERE
+    emp_no = NEW.emp_no;
+    
+    IF v_curr_salary IS NOT NULL THEN
+		UPDATE salaries
+        SET
+			to_date = SYSDATE()
+		WHERE
+			emp_no = NEW.emp_no AND to_date = NEW.to_date;
+	
+		INSERT INTO salaries
+			VALUES(NEW.emp_no, v_curr_salary + 20000, NEW.from_date, NEW.to_date);
+	END IF;
+END$$
+
+DELIMITER ;
+
+COMMIT;
+-- indefinite term contract (9999-01-01)
+INSERT INTO employees.dept_manager VALUES('111534', 'd009', DATE_FORMAT(SYSDATE(), '%Y-%m-%d'), '9999-01-01');
+
+SELECT *
+FROM employees.salaries
+WHERE emp_no = '111534'
+ORDER BY 2 DESC;
+
+SELECT *
+FROM dept_manager;
+
+COMMIT;
+
+DELETE FROM employees.dept_manager
+WHERE 
+	emp_no = '111534' AND
+	dept_no = 'd009' AND
+    from_date =  DATE_FORMAT(SYSDATE(), '%Y-%m-%d') AND
+    to_date = '9999-01-01';
+
+
+
+
+
+
+
