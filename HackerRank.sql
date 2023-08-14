f0288055c55fd66e7c19eb26e0631bd77caf9d58
INSUB KIM <42757351+slothpunch@users.noreply.github.com>
2023-08-14 21:29:28 +1000

    Add files via upload

--- HackerRank.sql
+++ HackerRank.sql
@@ -151,3 +151,25 @@ SELECT
         AS NUMERIC(20, 4)
         )
 FROM STATION
+
+-- ############################################################
+-- Q name: New Companies
+-- Diff: Medium 
+-- Date: 14 August 2023
+-- ########################
+
+SELECT 
+    c.company_code, 
+    founder, 
+    COUNT(DISTINCT lm.lead_manager_code),
+    COUNT(DISTINCT sm.senior_manager_code),
+    COUNT(DISTINCT m.manager_code),
+    COUNT(DISTINCT e.employee_code)
+FROM company c
+JOIN lead_manager lm ON c.company_code = lm.company_code
+JOIN senior_manager sm ON lm.lead_manager_code = sm.lead_manager_code
+JOIN manager m ON sm.senior_manager_code = m.senior_manager_code
+JOIN employee e ON m.manager_code = e.manager_code
+GROUP BY c.company_code, founder
+ORDER BY c.company_code
+;
